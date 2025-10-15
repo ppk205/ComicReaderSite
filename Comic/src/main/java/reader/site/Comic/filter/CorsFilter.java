3098c1cecf5d99708f84771e3090858e7d568fe2
mhuyyho <mhuyy.ho@gmail.com>
2025-10-16 02:41:36 +0700

    CORS + Mail
    CORS + Mail

--- Comic/src/main/java/reader/site/Comic/filter/CorsFilter.java
+++ Comic/src/main/java/reader/site/Comic/filter/CorsFilter.java
@@ -1,30 +1,32 @@
 package reader.site.Comic.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter("/*")
 public class CorsFilter implements Filter {
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse resp = (HttpServletResponse) response;
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
         HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
 
-        String origin = req.getHeader("Origin");
-        if (origin == null) origin = "http://localhost:3000";
-        resp.setHeader("Access-Control-Allow-Origin", origin);
-        resp.setHeader("Vary", "Origin");
-        resp.setHeader("Access-Control-Allow-Credentials", "true");
-        resp.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
-        resp.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-
+        res.setHeader("Access-Control-Allow-Origin", "https://comicreadersite.azurewebsites.net");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        res.setHeader("Access-Control-Expose-Headers", "Authorization");
         if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            res.setStatus(HttpServletResponse.SC_OK);
             return;
         }
 
         chain.doFilter(request, response);
     }
 }
+
